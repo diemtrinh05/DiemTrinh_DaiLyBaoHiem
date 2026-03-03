032044ab5aeb92d56ca1e49022a54d1cae1ca5c1
diemtrinh <diemtrinhdao05@gmail.com>
2026-03-03 16:55:21 +0700

    Final lab4 - microservices setup completed

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,8 +1,10 @@
+﻿using MediatR;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMediatR(typeof(Program));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,3 +25,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
