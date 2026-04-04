c73a63ba3f8b2b629c56def47f7e9ba08637d526
diemtrinh <diemtrinhdao05@gmail.com>
2026-04-04 08:44:02 +0700

    Finish Lab7

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,13 +1,28 @@
-﻿using MediatR;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using ProductService.DataAccess.EF;
+using ProductService.Init;
+using Steeltoe.Discovery.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddMediatR(typeof(Program));
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.Converters.Add(new StringEnumConverter());
+        options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDiscoveryClient(builder.Configuration);
+builder.Services.AddEFConfiguration(builder.Configuration);
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+builder.Services.AddProductDemoInitializer();
 
 var app = builder.Build();
 
@@ -24,6 +39,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseInitializer();
+
 app.Run();
 
-
+public partial class Program
+{
+}
